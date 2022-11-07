f65772a44a5ac2efdcb9aa869804ad5ee642c029
umarmansoor001 <i190594@nu.edu.pk>
2022-11-07 12:48:00 +0500

    Connection and data fetching from web api

--- app/src/main/java/com/umarmansoor/class_task/MainActivity.java
+++ app/src/main/java/com/umarmansoor/class_task/MainActivity.java
@@ -9,6 +9,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,35 +35,48 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        rv=findViewById(R.id.rv);
+        rv = findViewById(R.id.rv);
         add = findViewById(R.id.add);
         ls = new ArrayList<>();
 
+        adapter = new MyAdapter(ls, MainActivity.this);
+        RecyclerView.LayoutManager lm = new LinearLayoutManager(MainActivity.this);
+        rv.setLayoutManager(lm);
+        rv.setAdapter(adapter);
 
+        // database work
+        RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+        StringRequest request = new StringRequest(Request.Method.GET, "",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
 
-        add.setOnClickListener(new View.OnClickListener() {
+                        try {
+                            JSONObject res = new JSONObject(response);
+                            if (res.getInt("code") == 1) {
+                                JSONArray contacts = res.getJSONArray("contacts");
+                                for (int i = 0; i < contacts.length(); ++i) {
+                                    JSONObject obj = contacts.getJSONObject(i);
+                                    ls.add(new MyModel(obj.getString("name"), obj.getString("phno"), obj.getString("address")));
+                                    adapter.notifyDataSetChanged();
+                                }
+                            } else {
+                                Toast.makeText(MainActivity.this, res.getInt("msg"), Toast.LENGTH_SHORT).show();
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, add.class);
-                startActivityForResult(intent,2);
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(MainActivity.this, "JSON error", Toast.LENGTH_SHORT).show();
             }
-        });
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == 2)
-        {
-            String name = data.getStringExtra("NAM");
-            String number = data.getStringExtra("NUM");
-            String address = data.getStringExtra("ADDRES");
-
-            ls.add(new MyModel(name,number,address));
-            adapter=new MyAdapter(ls,MainActivity.this);
-            RecyclerView.LayoutManager lm = new LinearLayoutManager(MainActivity.this);
-            rv.setLayoutManager(lm);
-            rv.setAdapter(adapter);
         }
+
+        );
+        queue.add(request);
     }
+
 }
